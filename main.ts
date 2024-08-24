b2474336cbe499f1d2a0cfe6ebed96625bd1e97b
sidra farooq <162590786+sidrafarooqq@users.noreply.github.com>
2024-08-24 21:15:42 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,39 @@
+import inquirer from "inquirer"
+
+
+import { differenceInSeconds} from "date-fns";
+
+
+const resp = await inquirer.prompt({
+    type: "number",
+    name: "userInput",
+    message: "Please enter the amount of second",
+    validate: (input)=>{
+        if (isNaN(input)){
+            return "Please enter valid number"
+        }else if (input > 60){
+            return "Seconds must be in 60 or under 60"
+        }else{
+            return true
+        }
+    }
+});
+
+let input = resp.userInput
+function startTime(val:number){
+    const intTime = new Date().setSeconds(new Date().getSeconds()+ val);
+    const intervalTime = new Date(intTime);
+    setInterval(()=>{
+        const currentTime = new Date()
+        const timeDiff = differenceInSeconds(intervalTime, currentTime);
+        if(timeDiff <= 0){
+            console.log("Timer has expired");
+            process.exit()
+        }
+        const min = Math.floor((timeDiff%(3600*24))/3600)
+        const sec = Math.floor(timeDiff%60)
+        console.log(`${min.toString().padStart(2,"0")}:${sec.toString().padStart(2,"0")}`)
+    },1000);
+}
+
+startTime(input)
